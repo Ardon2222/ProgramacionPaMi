3fc1ea74e47c31be098c4ddcb4c49e804f0c7453
Eduard Elenchev Georgiev <eduard.s433238@cesurformacion.com>
2024-10-25 08:54:48 +0200

    ExamenSorpresa

---
+++ .ExamenSorpresa/ExMax.java
@@ -0,0 +1,11 @@
+package Trabajo;
+
+public class ExMax {
+    public static int ExMax(int num1, int num2) {
+        if (num1>=num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
+}
